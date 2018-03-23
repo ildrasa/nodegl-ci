cd9ed9eabdedc3f28135e187f712de4950d1dd68
ildrasa <ildrasa@kalash-PC.home>
2018-03-23 17:10:33 +0100

    [fix] fix permission problems with docker volumes, move to use --mount type=bind in docker run command

--- nodegl-sh-ci.sh
+++ nodegl-sh-ci.sh
@@ -38,42 +38,30 @@ if [[ "$(docker images -q nodegl:CI 2> /dev/null)" == "" ]]; then
 	docker build -f nodegl-dockerfile -t nodegl:CI .
 fi
 
-
-echo "manage docker volumes"
-# get "coverity" docker volume path; create it if it does not exists yet
-COVERITY_PATH=$(docker volume inspect --format '{{ .Mountpoint }}' coverity)
-if [[ -z "$COVERITY_PATH" ]]; then
-	echo "Docker volume 'coverity' not found. It will de be created"
-	docker volume create coverity
-	COVERITY_PATH=$(docker volume inspect --format '{{ .Mountpoint }}' coverity)
+COVERITY_PATH=coverity
+if [[ ! -d $COVERITY_PATH ]]; then
+	mkdir $COVERITY_PATH
 	# get coverity tarball
 	wget https://scan.coverity.com/download/linux64 --post-data "token=$TOKEN&project=node.gl" -O coverity_tool.tgz
 	echo "untar coverity into volume 'coverity' "
 	# untar coverity into coverity volume
 	tar -xvzf coverity_tool.tgz -C $COVERITY_PATH
-	echo "Docker volume 'coverity' created at $COVERITY_PATH."
-else
-	echo "Docker volume 'coverity' found at $COVERITY_PATH."
 fi
 
-
-
-# get "build-script" docker volume path; create it if it does not exists yet
-BUILD_SCRIPT_PATH=$(docker volume inspect --format '{{ .Mountpoint }}' build-script)
-if [[ -z "$BUILD_SCRIPT_PATH" ]]; then
-	echo "Docker volume 'build-script' not found. It will de be created"
-	docker volume create build-script
-	BUILD_SCRIPT_PATH=$(docker volume inspect --format '{{ .Mountpoint }}' build-script)
-	# copy nodegl build script into build-script volume and give it execution rights
-	echo "copy cnodegl build script into volume 'build-script' "
-	cp nodegl-build.sh $BUILD_SCRIPT_PATH
-	chmod -x $BUILD_SCRIPT_PATH/nodegl-build.sh
+BUILD_SCRIPT_PATH=nodegl-build
+if [[ ! -d $BUILD_SCRIPT_PATH ]]; then
+	mkdir $BUILD_SCRIPT_PATH
+	copy nodegl build script into build-script volume and give it execution rights
+ 	echo "copy cnodegl build script into volume 'build-script' "
+ 	cp nodegl-build.sh $BUILD_SCRIPT_PATH
+ 	chmod -x $BUILD_SCRIPT_PATH/nodegl-build.sh
 fi
-echo "Docker volume 'build-script' found at $BUILD_SCRIPT_PATH."
+
 
 # start docker container used for build and running tests
 echo "start container"
-docker run -d --name nodegl-build -t --mount source=coverity,destination=/home/root/coverity --mount source=build-script,destination=/home/root/build-script nodegl:CI -e TOKEN=$TOKEN -e MAIL=$MAIL
+ABSOLUTE_PATH=$(pwd)
+docker run -d --name nodegl-build -t --mount type=bind,src=$ABSOLUTE_PATH/$COVERITY_PATH,dest=/home/root/coverity --mount type=bind,src=$ABSOLUTE_PATH/$BUILD_SCRIPT_PATH,destination=/home/root/build-script nodegl:CI -e TOKEN=$TOKEN -e MAIL=$MAIL
 
 # launch build and tests
 echo "launch build and test on container"
@@ -86,7 +74,7 @@ cp -R $BUILD_SCRIPT_PATH/tests-results .
 tar cvzf cov-int.tgz cov-int
 
 # upload results
-echo "upload result to coverity scan
+echo "upload result to coverity scan"
 curl --form token=$TOKEN \
   --form email=$MAIL \
   --form file=@cov-int.tgz \
